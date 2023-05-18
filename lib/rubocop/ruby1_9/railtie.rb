cd72a5ea0fbf1bd9f522b6f301b725d2143a42c7
Peter Boling <peter.boling@gmail.com>
2023-05-19 00:13:30 +0700

    🚑️ Ship rakelib/**/*.rake tasks
    - Fix rake_tasks hook on Railtie

--- lib/rubocop/ruby1_9/railtie.rb
+++ lib/rubocop/ruby1_9/railtie.rb
@@ -1,10 +1,14 @@
+puts "HELLO I AM RAILTIE in #{Rails.env}"
 module Rubocop
   module Ruby19
     class Railtie < Rails::Railtie
       railtie_name :rubocop_ruby1_9
 
-      rake_tasks do
-        load "rubocop/ruby1_9/tasks.rake" if Rails.env.test? || Rails.env.development?
+      if Rails.env.test? || Rails.env.development?
+        rake_tasks do
+          path = File.expand_path(__dir__)
+          Dir.glob("#{path}/rakelib/**/*.rake").each { |f| load f }
+        end
       end
     end
   end
